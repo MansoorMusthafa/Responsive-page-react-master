3e024c1de5864ac96ec676cd9ca882f5d93b2cf2
Mansoor Musthafa <msmansoor523@gmail.com>
2024-10-03 16:44:20 +0530

    new chnages

--- src/routes/PersonalInfo.js
+++ src/routes/PersonalInfo.js
@@ -10,11 +10,11 @@ function PersonalInfo() {
           <div className="col-sm-10 col-md-8 details">
             <h1 className="name">Hi, I'm Mansoor Musthafa</h1>
             <p className="intro">
-              I'm a Frontend developer with 11 months of experience using React,
+              I'm a Frontend developer with 1.9 years of experience using Html,Css, Javascript & React.js.
               Reach out if you'd like to learn more!
             </p>{" "}
             <button className="btn">
-              {" "}
+           
               <FaPhoneAlt />
               6385126545
             </button>
